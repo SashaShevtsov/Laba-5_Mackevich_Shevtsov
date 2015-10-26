76c814450e0a72adda85c838121e3e55129a3e06
Alex Shevtsov <Shavo4kin@gmail.com>
2015-10-27 00:20:08 +0300

    Added tests for Task2

--- task2.cpp
+++ task2.cpp
@@ -64,8 +64,31 @@ int babyDontHurtMe(char* str)
 	}
 	return a[0];
 }
-
+void assert(char* string, int answer)
+{
+	int firstanswer = babyDontHurtMe(string);
+	if (firstanswer == answer)
+		cout << "OK" << endl;
+	else
+		cout << "False" << endl;
+}
+void tests()
+{
+	char string1[] = "9";
+	char string2[] = "max(5,min(4,2))";
+	char string3[] = "max(5,max(min(max(5,8),7),min(6,max(3,max(2,1)))))";
+	char string4[] = "min(9,min(8,min(7,max(0,6))))";
+	char string5[] = "min(min(max(8,7),max(9,0)),max(min(0,2),min(3,1)))";
+	char string6[] = "max(min(max(0,0),8),0)";
+	assert(string1, 9);
+	assert(string2, 5);
+	assert(string3, 7);
+	assert(string4, 6);
+	assert(string5, 3);
+	assert(string6, 0);
+}
 int main()
 {
+	tests();
 	return 0;
 }
